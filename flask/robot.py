88399b2357adf61186852a284b492dc359ab4da8
kolwinb <kolwinmail@gmail.com>
2021-03-21 22:48:36 +0530

    fixed bug

--- flask/robot.py
+++ flask/robot.py
@@ -15,6 +15,7 @@ def runRobot(BatCap):
 	print ("Current Date :{}".format(now))
 	print ("hour :{}, minute:{}, second:{}".format(now.hour,now.minute,now.second))
 	if (( now.hour >= 8 ) and (now.hour < 15)):
+		checkOtherSwitch()
 		if ( BatCap < 52 ):
 			print ("info : Battery Low ({}%)".format(BatCap))
 			#send signal to inverter turn off (ongrid on)
@@ -23,22 +24,25 @@ def runRobot(BatCap):
 			print ("info : Battery High ({}%)".format(BatCap))
 			#send signal to inverter turn on (ongrid off)
 			RpiPin().setPinState({"inverter":1})
-	elif (( now.hour == 18 ) and (now.minute == 28)):
+	elif (( now.hour == 18 ) and (now.minute == 28) and ( now.second == 0 )):
 		#time off (6:28 pm)
-		switchon.setPinRelay(rx570)
-		time.sleep(5)
-		switchon.setPin(pc)
-		switchon.setPin(ac)
-	elif (( now.hour == 22 ) and (now.minute == 31)):
-		#time on (10:31 pm)
+		print ("18:28 pm will shutdown")
 		switchoff.setPinRelay(rx570)
 		switchoff.setPin(pc)
 		switchoff.setPin(ac)
+	elif (( now.hour == 22 ) and ( now.minute == 31 ) and ( now.second == 0 )):
+		print ("22:31 pm started")
+		#time on (10:31 pm)
+		switchon.setPinRelay(rx570)
+		sleep(5)
+		switchon.setPin(pc)
+		switchon.setPin(ac)
 	else:
 		pinStatus=RpiPin().getPinStatus()
 		if (pinStatus["inverter_serial"] == 1):
 			#inverter will off then ongrid on state.
 			RpiPin().setPinState({"inverter":0})
+			checkOtherSwitch()
 
 def checkOtherSwitch():
 	pinStatus=RpiPin().getPinStatus()
@@ -48,7 +52,7 @@ def checkOtherSwitch():
 		RpiPin().setPinState({"pcpower":1})
 	elif (pinStatus["rx570x4"] == 0):
 		RpiPin().setPinState({"rx570x4":1})
-	print ("Current Status: {}".format(pinStatus))
+#	print ("Current Status: {}".format(pinStatus))
 
 while True:
 	ChgSts=esmart()
@@ -56,7 +60,7 @@ while True:
 	print ("Data : {}".format(chgData))
 	BatCap=chgData['BatCap']
 	#run other switch if did not run
-	checkOtherSwitch()
+	#checkOtherSwitch()
 	#automate base on battery level
 	runRobot(int(BatCap))
 	sleep(1)
